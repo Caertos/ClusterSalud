6677355f7339d105f8173908c21a4d418a3dd438
Caertos <cocheroc@gmail.com>
2024-12-05 20:08:06 -0500

    cambio menor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-20">
           <div className="flex-shrink-0 flex items-center">
-            <Link to="/">
+            <Link to="/ClusterSalud">
               <img
                 className="h-auto w-40 object-contain"
                 src="/ClusterSalud/logo.png"
@@ -20,7 +20,7 @@ const Navbar = () => {
           </div>
           
           <div className="hidden md:flex items-center space-x-8">
-            <Link to="/" className="text-gray-700 hover:text-primary transition">Inicio</Link>
+            <Link to="/ClusterSalud" className="text-gray-700 hover:text-primary transition">Inicio</Link>
             <Link to="/about" className="text-gray-700 hover:text-primary transition">Acerca de</Link>
             <Link to="/services" className="text-gray-700 hover:text-primary transition">Servicios</Link>
             <Link to="/projects" className="text-gray-700 hover:text-primary transition">Proyectos</Link>
